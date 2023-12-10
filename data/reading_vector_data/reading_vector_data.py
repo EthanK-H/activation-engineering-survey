29af3ae7c9eca1cbef39dcc1c23f10da2ee099ed
Robert McCarthy <robertmccarthy97@gmail.com>
2023-12-10 11:52:34 +0000

    add athropic  mcq dataset evals

---
+++ data/reading_vector_data/reading_vector_data.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+import random
+from transformers import PreTrainedTokenizer
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib.colors import LinearSegmentedColormap
+
+
+def honesty_function_dataset(data_path: str, tokenizer: PreTrainedTokenizer, user_tag: str = "", assistant_tag: str = "", seed: int = 0) -> (list, list):
+    """
+    Processes data to create training and testing datasets based on honesty.
+
+    Args:
+    - data_path (str): Path to the CSV containing the data.
+    - tokenizer (PreTrainedTokenizer): Tokenizer to tokenize statements.
+    - user_tag (str): Instruction template.
+    - assistant_tag (str): Instruction template user tag.
+    - seed (int): Random seed for reproducibility.
+
+    Returns:
+    - Tuple containing train and test data.
+    """
+
+    # Setting the seed for reproducibility
+    random.seed(seed)
+
+    # Load the data
+    df = pd.read_csv(data_path)
+    true_statements = df[df['label'] == 1]['statement'].values.tolist()
+    false_statements = df[df['label'] == 0]['statement'].values.tolist()
+
+    template_str = "Pretend you're {type} person making statements about the world."
+    honest_statements = []
+    untruthful_statements = []
+
+    # Process statements
+    for statement in true_statements:
+        tokens = tokenizer.tokenize(statement)
+        
+        for idx in range(1, len(tokens) - 5):
+            truncated_tokens = tokens[:idx]
+            truncated_statement = tokenizer.convert_tokens_to_string(truncated_tokens)
+
+            honest_statements.append(f"{user_tag} {template_str.format(type='an honest')} {assistant_tag} " + truncated_statement)
+            untruthful_statements.append(f"{user_tag} {template_str.format(type='an untruthful')} {assistant_tag} " + truncated_statement)
+
+    # Create training data
+    ntrain = 512
+    combined_data = [[honest, untruthful] for honest, untruthful in zip(honest_statements, untruthful_statements)]
+    train_data = combined_data[:ntrain]
+
+    train_labels = []
+    for d in train_data:
+        true_s = d[0]
+        random.shuffle(d)
+        train_labels.append([s == true_s for s in d])
+    
+    train_data = np.concatenate(train_data).tolist()
+
+    # Create test data
+    reshaped_data = np.array([[honest, untruthful] for honest, untruthful in zip(honest_statements[:-1], untruthful_statements[1:])]).flatten()
+    test_data = reshaped_data[ntrain:ntrain*2].tolist()
+
+    print(f"Train data: {len(train_data)}")
+    print(f"Test data: {len(test_data)}")
+
+    return {
+        'train': {'data': train_data, 'labels': train_labels},
+        'test': {'data': test_data, 'labels': [[1,0]] * len(test_data)}
+    }
+
+def sycophancy_function_dataset(data_path: str, tokenizer: PreTrainedTokenizer, user_tag: str = "", assistant_tag: str = "", seed: int = 0) -> (list, list):
+    """
+    Processes data to create training and testing datasets based on honesty.
+
+    Args:
+    - data_path (str): Path to the CSV containing the data.
+    - tokenizer (PreTrainedTokenizer): Tokenizer to tokenize statements.
+    - user_tag (str): Instruction template.
+    - assistant_tag (str): Instruction template user tag.
+    - seed (int): Random seed for reproducibility.
+
+    Returns:
+    - Tuple containing train and test data.
+    """
+
+    # Setting the seed for reproducibility
+    random.seed(seed)
+
+    # Load the data
+    df = pd.read_csv(data_path)
+    true_statements = df[df['label'] == 1]['statement'].values.tolist()
+    false_statements = df[df['label'] == 0]['statement'].values.tolist()
+
+    template_str = "Pretend you're {type} person making statements about the world."
+    honest_statements = []
+    untruthful_statements = []
+
+    # Process statements
+    for statement in true_statements:
+        tokens = tokenizer.tokenize(statement)
+        
+        for idx in range(1, len(tokens) - 5):
+            truncated_tokens = tokens[:idx]
+            truncated_statement = tokenizer.convert_tokens_to_string(truncated_tokens)
+
+            honest_statements.append(f"{user_tag} {template_str.format(type='an honest')} {assistant_tag} " + truncated_statement)
+            untruthful_statements.append(f"{user_tag} {template_str.format(type='a sycophantic')} {assistant_tag} " + truncated_statement)
+
+    # Create training data
+    ntrain = 512
+    combined_data = [[honest, untruthful] for honest, untruthful in zip(honest_statements, untruthful_statements)]
+    train_data = combined_data[:ntrain]
+
+    train_labels = []
+    for d in train_data:
+        true_s = d[0]
+        random.shuffle(d)
+        train_labels.append([s == true_s for s in d])
+    
+    train_data = np.concatenate(train_data).tolist()
+
+    # Create test data
+    reshaped_data = np.array([[honest, untruthful] for honest, untruthful in zip(honest_statements[:-1], untruthful_statements[1:])]).flatten()
+    test_data = reshaped_data[ntrain:ntrain*2].tolist()
+
+    print(f"Train data: {len(train_data)}")
+    print(f"Test data: {len(test_data)}")
+
+    return {
+        'train': {'data': train_data, 'labels': train_labels},
+        'test': {'data': test_data, 'labels': [[1,0]] * len(test_data)}
+    }
